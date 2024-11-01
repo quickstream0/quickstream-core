c22e631e37e8e58b51c7f85e41e2282751ff9a1c
quickstream0 <app.quickstream@gmail.com>
2024-11-01 20:49:58 +0300

    redeploy, fix db url

--- run.py
+++ run.py
@@ -1,13 +1,17 @@
 import os
-from app import create_app
+from app import create_app, db
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = create_app()
 
+# app.app_context().push()    
+# db.drop_all()
+# db.create_all()
+
 if __name__ == "__main__":
     host = os.getenv('HOST', '0.0.0.0')
     port = os.getenv('PORT', '5000')
     print(" * Running server")
-    app.run(debug=False, use_reloader=False, host=host, port=port)
+    app.run(debug=True, use_reloader=False, host=host, port=port)
