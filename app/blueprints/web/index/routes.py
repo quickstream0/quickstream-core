17071186896699cda89c0c66d8efd6b85fa2d53b
quickstream0 <app.quickstream@gmail.com>
2024-11-02 23:09:07 +0300

    re-fix download

--- app/blueprints/web/index/routes.py
+++ app/blueprints/web/index/routes.py
@@ -1,5 +1,4 @@
-import requests
-from flask import render_template, Response
+from flask import render_template
 
 from . import index_bp
 
@@ -11,21 +10,3 @@ def index():
 def pricing():
     return render_template('pricing.html')
 
-@index_bp.route('/download')
-def download():
-    url = "https://github.com/quickstream0/quickstream-core/releases/download/v1.0.0/quickstream-v1.0.0.apk"
-
-    # Send request to GitHub to fetch the file
-    response = requests.get(url, stream=True)
-    
-    if response.status_code == 200:
-        # Send the file to the client
-        return Response(
-            response.iter_content(chunk_size=8192),
-            headers={
-                'Content-Disposition': 'attachment; filename="quickstream-v1.0.0.apk"',
-                'Content-Type': 'application/octet-stream'
-            }
-        )
-    else:
-        return "Failed to download file", response.status_code
--- run.py
+++ run.py
@@ -9,4 +9,4 @@ app = create_app()
 if __name__ == "__main__":
     host = os.getenv('HOST', '0.0.0.0')
     port = os.getenv('PORT', '5000')
-    app.run(debug=True, host=host, port=port)
+    app.run(host=host, port=port)
