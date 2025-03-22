8856e7d7f78836ffdf01d31463c4dc977e9ca6bc
quickstream0 <app.quickstream@gmail.com>
2025-03-22 12:49:28 +0300

    delete unecessary code

--- run.py
+++ run.py
@@ -1,14 +1,10 @@
 import os
-from app import create_app, db
+from app import create_app
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = create_app()
-
-# app.app_context().push()  
-# db.drop_all()
-# db.create_all()
 
 if __name__ == "__main__":
     host = os.getenv('HOST', '0.0.0.0')
