1f368eff57831c0c2094e019609300019475ba02
quickstream0 <app.quickstream@gmail.com>
2025-03-25 17:39:43 +0300

    add contact form

--- app/blueprints/web/index/routes.py
+++ app/blueprints/web/index/routes.py
@@ -10,6 +10,10 @@ def index():
 def pricing():
     return render_template('pricing.html')
 
+@index_bp.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @index_bp.route('/faq')
 def faq():
     return render_template('faq.html')
