2f97651e52bf324fe282c589f55a974088fc1762
quickstream0 <app.quickstream@gmail.com>
2026-01-11 17:10:38 +0300

    edit sitemap

--- app/blueprints/web/seo/routes.py
+++ app/blueprints/web/seo/routes.py
@@ -21,12 +21,16 @@ def sitemap():
     ]
 
     xml = ['<?xml version="1.0" encoding="UTF-8"?>']
-    xml.append('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">')
+    xml.append(
+        '''<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">''')
 
     for page in pages:
         xml.append('  <url>')
         xml.append(f'    <loc>{page}</loc>')
-        xml.append('    <lastmod>2025-01-11</lastmod>')
+        xml.append('    <lastmod>2026-01-11</lastmod>')
         xml.append('    <changefreq>monthly</changefreq>')
         xml.append('    <priority>0.9</priority>')
         xml.append('  </url>')
